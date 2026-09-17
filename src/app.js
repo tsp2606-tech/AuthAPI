7ea32e884f783f3979356844ca48439ac6e50bf7
tsp2606-tech <tsp26062007@gmail.com>
2026-09-17 20:56:22 +0700

    feat: initialize express application with CORS, Swagger documentation, and auth routes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://auth-fe-thaisonpham26.vercel.app/"
+    "https://auth-fe-thaisonpham26.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
